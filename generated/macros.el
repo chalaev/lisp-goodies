cf21927f5cc6318811b6aa7460cd32330fd005d0
Oleg Shalaev <oleg@leanws.com>
2020-11-02 09:08:49 -0500

    1. generalized "case=" to "case*" 2. updated "when-set", "needs" and "needs-set" 3. added "unless-set"

--- generated/macros.el
+++ generated/macros.el
@@ -27,11 +27,23 @@
 
 (defmacro when-set (vars &rest body)
   "when-let using global variable instead of defining local one"
-  `(progn (setf ,(caar vars) ,(cadar vars)); get rid of progn here
+(let ((GV (gensym)))
+  `(let ((,GV ,(cadar vars)))
      ,(if (cdr vars)
-	  `(when ,(caar vars)
+	  `(when ,GV
+              (setf ,(caar vars) ,GV)
 	     ,(macroexpand-1 `(when-set ,(cdr vars) ,@body)))
-	(append `(when ,(caar vars)) body))))
+	(append `(when ,GV (setf ,(caar vars) ,GV)) body)))))
+
+(defmacro unless-set (vars &rest body)
+  "unless-let using global variable instead of defining local one"
+(let ((GV (gensym)))
+  `(let ((,GV ,(cadar vars)))
+     ,(if (cdr vars)
+	  `(if ,GV
+              (setf ,(caar vars) ,GV)
+	     ,(macroexpand-1 `(unless-set ,(cdr vars) ,@body)))
+	(append `(if ,GV (setf ,(caar vars) ,GV)) body)))))
 
 (defmacro if-let (vars &rest body)
   "if with let using stndard let-notation"
@@ -55,12 +67,12 @@
     ,(car body)))
 
 (defmacro if-set (vars &rest body)
-  (let ((if-true (gensym "it")))
-    `(let (,if-true)
-       (when-set ,vars
+  (let ((if-true (gensym "it")) (result (gensym "r")))
+    `(let (,if-true ,result)
+       (setf ,result (when-set ,vars
 		  (setf ,if-true t)
-		  ,(car body))
-       (unless ,if-true
+		  ,(car body)))
+       (if ,if-true ,result
 	 ,@(cdr body)))))
 
 (defmacro cond-let (&rest conds)
@@ -71,11 +83,9 @@
 	`(if-let ,(car c) ,(cons 'progn (cdr c)) ,(macroexpand-1 `(cond-let ,@r)))
 	`(when-let ,(car c) ,@(cdr c))))))
 
-(defmacro needs (&rest all-args)
+(defmacro needs (vardefs &rest body)
   "unifying when-let and if-let"
-  (let* ((vardefs (car all-args))
-	(body (cdr all-args))
-	(vardef (car vardefs)))
+  (let ((vardef (car vardefs)))
     (if (and (listp vardef) (not (or (special-form-p (car vardef)) (functionp (car vardef)) (macrop (car vardef)))))
     `(let ((,(car vardef) ,(cadr vardef)))
        ,(if (cddr vardef)
@@ -93,27 +103,20 @@
 		      (list (macroexpand-1 `(needs ,(cdr vardefs) ,@body)))
 		    body)))))
 
-(defmacro needs-set (&rest all-args)
+(defmacro first2(ll) `(firstN ,ll 2))
+(defmacro needs-set (vardefs &rest body)
   "needs with 'let' being replaced with 'setf'"
-  (let* ((vardefs (car all-args))
-	(body (cdr all-args))
-	(vardef (car vardefs)))
-    (if (and (listp vardef) (not (or (special-form-p (car vardef)) (functionp (car vardef)) (macrop (car vardef)))))
-    `(progn (setf ,(car vardef) ,(cadr vardef))
-       ,(if (cddr vardef)
-	    `(if ,(car vardef)
-		,(if (cdr vardefs)
-		     (macroexpand-1 `(needs ,(cdr vardefs) ,@body))
-		   `(progn ,@body))
-	       ,(car (cddr vardef)))
-	  (append `(when ,(car vardef))
-		  (if (cdr vardefs)
-		      (list (macroexpand-1 `(needs ,(cdr vardefs) ,@body)))
-		    body))))
-    (append `(when ,vardef)
-		  (if (cdr vardefs)
-		      (list (macroexpand-1 `(needs ,(cdr vardefs) ,@body)))
-		    body)))))
+  (let ((vardef (car vardefs)))
+    (if (cddr vardef)
+      `(if-set (,(first2 vardef))
+	  ,(if (cdr vardefs)
+	       (macroexpand-1 `(needs-set ,(cdr vardefs) ,@body))
+	     (cons 'progn body))
+	  ,(caddr vardef))
+      `(when-set (,(car vardefs))
+	   ,(if (cdr vardefs)
+	       (macroexpand-1 `(needs ,(cdr vardefs) ,@body))
+	      (cons 'progn body))))))
 
 (defmacro ifn (test ifnot &rest ifyes)
 `(if (not ,test) ,ifnot ,@ifyes))
